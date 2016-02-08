2da243b1f5d4f85d61859086c28751997569f20a
Duane Allam <duane.allam@gmail.com>
2016-02-08 12:43:14 +1100

    asdfsfsf

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', host: req.hostname, request: JSON.stringify(req) });
+  res.render('index', { title: 'Express', host: req.hostname, protocol: req.protocol });
 });
 
 module.exports = router;
