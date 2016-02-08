1ae9de1f530e44d5391f615e9c81f25171b2cb85
Duane Allam <duane.allam@gmail.com>
2016-02-08 12:23:07 +1100

    dump request object to index for to work out protocol differences...

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', host: req.hostname });
+  res.render('index', { title: 'Express', host: req.hostname, request: JSON.stringify(req) });
 });
 
 module.exports = router;
