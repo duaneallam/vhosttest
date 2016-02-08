cc1d49f0c43930f684157bb6de49a725612910d5
Duane Allam <duane.allam@gmail.com>
2016-02-08 13:08:26 +1100

    check photo vs x-forwarded-proto

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', host: req.hostname, headers: JSON.stringify(req.headers) });
+  res.render('index', { 
+  	title: 'Express', 
+  	host: req.hostname, 
+  	proto: req.protocol,
+  	x_proto: req.headers["x-forwarded-proto"]
+  });
 });
 
 module.exports = router;
