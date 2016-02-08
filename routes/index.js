2006f4b27c6d6c9f380b0806ddbf9c2d7fe79f86
Duane Allam <duane.allam@gmail.com>
2016-02-08 12:56:31 +1100

    lets look at the headers...

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', host: req.hostname, protocol: req.protocol });
+  res.render('index', { title: 'Express', host: req.hostname, headers: JSON.stringify(req.headers) });
 });
 
 module.exports = router;
